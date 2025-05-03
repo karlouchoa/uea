c93890ee2ec4b17f092693b32cd00de9dd3dd64d
karlouchoa <karlouchoa@gmail.com>
2025-05-03 10:44:14 -0400

    Prisma generate

--- backend/main.js
+++ backend/main.js
@@ -1,18 +1,45 @@
-const express = require('express')
+const express = require('express'); // Importando o express
+const cors = require('cors'); // Importando o cors para permitir requisições de outros domínios
+require('dotenv').config(); // Importando o dotenv para variáveis de ambiente
+const Prisma = require('./prisma/client'); // Importando o Prisma Client
 
-require('dotenv').config()
-const cors = require('cors')
+
+const app = express(); // Criando uma instância do express
+app.use(express.json()); // Middleware para trabalhar com JSON
+app.use(cors()); // Middleware para permitir requisições de outros domínios
 
 
-const app = express()
-app.use('cors')
-app.use(express.json())
+app.get('/', (req, res) => { // Rota raiz
+    res.send('Hello World!'); // Retorrna "Hello World!" quando acessa a rota raiz
+    }
+);
 
-app.get('/', (req,res) => {
-    res.send('Hello Word')
+app.get('api/usuarios', async (req, res) => { 
+    const usuarios = await Prisma.user.findMany(    )
+    const listUsuarios = usuarios.map(usuario => {
+        return {
+            id: usuario.id,
+            name: usuario.name,
+            email: usuario.email
+        }
+    })
+    res.json(listUsuarios) 
+}
+)
+
+app.post('/api/usuarios', async (req, res) => {
+    const { name, email, password } = req.body
+    const usuario = await Prisma.user.create({
+        data: {
+            name,
+            email,
+            password
+        }
+    })
+    res.json(usuario) 
 })
 
-
-app.listen(3000, () =>{
-   console.log('Servidor rodando na porta 3000')
-});
+app.listen(process.env.PORT || 3000, () => { // Ligando o servidor na porta 3000
+    console.log('Servidor rodando na porta 3000');
+    }
+);
